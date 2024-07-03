7c5850a6138f5539faa69b71e3d28fc7dd293ff9
Davis Tibbz <davisinyo@gmail.com>
2024-07-03 13:28:15 +0800

    new method awayFromEdge for given position

--- room/enemy_detect.go
+++ room/enemy_detect.go
@@ -6,6 +6,17 @@ import (
 	"log"
 )
 
+// isEvenCellRow determines wheter given cell_index is on even Row
+func isEvenCellRow(cellIdx int32) bool {
+	rowNumber := 9 - (cellIdx-1)/4
+	return (rowNumber%2 == 0)
+}
+
+// awayFromEdge returns TRUE if given position is NOT on any edge of board
+func awayFromEdge(pos *game.Vec2) bool {
+	return pos.X > 0 && pos.X < 7*game.SIZE_CELL && pos.Y > 0 && pos.Y < 7*game.SIZE_CELL
+}
+
 // hasExtraTargets returns TRUE if there are nearby targets (4 sides of cellIdx) for player `p` to capture. (NOT entire board)
 func hasExtraTargets(p *player.Player, cellIdx int32, gameMap map[int32]*game.Piece) bool {
 	//var hasExtra = false
@@ -20,12 +31,6 @@ func hasExtraTargets(p *player.Player, cellIdx int32, gameMap map[int32]*game.Pi
 	}
 
 	return false
-}
-
-// isEvenCellRow determines wheter given cell_index is on even Row
-func isEvenCellRow(cellIdx int32) bool {
-	rowNumber := 9 - (cellIdx-1)/4
-	return (rowNumber%2 == 0)
 }
 
 // collectFrontLHS checks whether there is an enemy on LHS of this player `p`
@@ -60,16 +65,18 @@ func collectFrontLHS(p *player.Player, cellIdx int32, gameMap map[int32]*game.Pi
 	}
 
 	pieceAhead, existFront := gameMap[cellAheadIdx] // north west
-	log.Println("existFront", existFront, "i dont won", !p.HasThisPiece(pieceAhead.Id))
 	hasEnemyAhead = existFront && !p.HasThisPiece(pieceAhead.Id)
+	if existFront && !awayFromEdge(&pieceAhead.Pos) {
+		return false
+	}
 
 	cellBehindEnemy := cellIdx + (deltaBehindEnemy * mSign) + (deltaForward * mSign) // south east
 	if cellBehindEnemy > 32 || cellBehindEnemy < 1 {
 		return false
 	}
+
 	// does enemy piece have EMPTY cell behind it?
 	_, existBack := gameMap[cellBehindEnemy]
 	enemyOpenBehind = !existBack
-	log.Println("hasEnemyAhead", hasEnemyAhead, "enemyOpenBehind", enemyOpenBehind)
 	return hasEnemyAhead && enemyOpenBehind
 }
