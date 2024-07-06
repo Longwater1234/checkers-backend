7d43b8c7e10d048a9f8c6ef908086ebc122098bb
Davis T <davisinyo@gmail.com>
2024-07-06 23:49:21 +0800

    FIXED #8

--- room/capture_validator.go
+++ room/capture_validator.go
@@ -32,7 +32,7 @@ func processCapturePiece(basePayload *game.BasePayload, gameMap map[int32]*game.
 	return true
 }
 
-// validateCapture and updates map if VALID when player `p` attacks by opponent's piece
+// validateCapture when player `p` attacks by opponent's piece. returns TRUE if valid, else FALSE
 func validateCapture(captureReq *game.CapturePayload, gameMap map[int32]*game.Piece, opponent *player.Player) bool {
 	if captureReq.GetDetails() == nil || captureReq.GetHunterDestCell() == nil {
 		return false
