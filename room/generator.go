9ee88d69734342eacebed7be2ea30fbf4d3baa0d
Davis Tibbz <davisinyo@gmail.com>
2024-12-26 20:12:02 +0300

    when generateMap, skip even number cells

--- room/generator.go
+++ room/generator.go
@@ -25,32 +25,34 @@ func generateGameMap(p1 *player.Player, p2 *player.Player) map[int32]*game.Piece
 	// create pieces, and position them on checkerboard (from top -> down)
 	for row := 0; row < numRows; row++ {
 		for col := 0; col < numCols; col++ {
-			if (row+col)%2 != 0 {
-				pos := game.Vec2{
-					X: float32(col%numRows) * game.SIZE_CELL,
-					Y: float32(row) * game.SIZE_CELL,
-				}
-				if row < 3 && iterBlack != len(p2.Pieces) {
-					//BLACK PIECES
-					gameMap[counter] = &game.Piece{
-						Id:         p2.Pieces[iterBlack],
-						IsKing:     false,
-						Pos:        pos,
-						PieceColor: game.Piece_Black,
-					}
-					iterBlack++
-				} else if row > 4 && iterRed != len(p1.Pieces) {
-					//RED PIECES
-					gameMap[counter] = &game.Piece{
-						Id:         p1.Pieces[iterRed],
-						IsKing:     false,
-						Pos:        pos,
-						PieceColor: game.Piece_Red,
-					}
-					iterRed++
-				}
-				counter--
+			if (row+col)%2 == 0 {
+				// skip even-numbered cells
+				continue
 			}
+			pos := game.Vec2{
+				X: float32(col%numRows) * game.SIZE_CELL,
+				Y: float32(row) * game.SIZE_CELL,
+			}
+			if row < 3 && iterBlack != len(p2.Pieces) {
+				//BLACK PIECES
+				gameMap[counter] = &game.Piece{
+					Id:         p2.Pieces[iterBlack],
+					IsKing:     false,
+					Pos:        pos,
+					PieceColor: game.Piece_Black,
+				}
+				iterBlack++
+			} else if row > 4 && iterRed != len(p1.Pieces) {
+				//RED PIECES
+				gameMap[counter] = &game.Piece{
+					Id:         p1.Pieces[iterRed],
+					IsKing:     false,
+					Pos:        pos,
+					PieceColor: game.Piece_Red,
+				}
+				iterRed++
+			}
+			counter--
 		}
 	}
 	return gameMap
