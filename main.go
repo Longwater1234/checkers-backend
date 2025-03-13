33da2ed56900ed2f5ab0c8241d95814acc3e0b9b
Davis Tibbz <davisinyo@gmail.com>
2025-03-13 22:00:29 +0300

    log by url.host

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 	port := strconv.Itoa(portNum)
 
 	http.HandleFunc("/", func(writer http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(writer, `<p>This is a websocket server. Dial ws://%s:%s/game </p>`, r.RemoteAddr, port)
+		fmt.Fprintf(writer, `<p>This is a websocket server. Dial ws://%s:%s/game </p>`, r.URL.Host, port)
 	})
 
 	http.Handle("/game", websocket.Handler(wsHandler))
